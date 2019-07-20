a78990c641f23dd1bd70656b14047d52fb34f47b
Stella Kim <testcodesk@gmail.com>
2019-07-20 11:48:20 -0400

    1st attempt

---
+++ scripts.js
@@ -0,0 +1,16 @@
+<html>
+    <head>
+    <meta charset="utf-8"></meta>
+    </head>
+    <body>
+
+    </body>
+</html>
+<script>
+    let rowNo = 3;
+    let colNo = 3;
+    const grid =[];
+
+
+
+</script>
